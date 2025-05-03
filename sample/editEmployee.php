8989190fdcbf255b36b8a00d4ddf9d47a5ec40d2
Andrea Laserna <aalaserna@up.edu.ph>
2025-05-03 20:36:38 +0800

    edit employee update 2

--- sample/editEmployee.php
+++ sample/editEmployee.php
@@ -1,31 +1,30 @@
 <?php
-
 include 'DBConnector.php';
-// update submission
-if (isset($_POST['EmpID'])) {
-    $EmpID = $_POST['EmpID'];
-    $name = $_POST['EmpName'];
-    $age = $_POST['Age'];
-    $salary = $_POST['Salary'];
-    $hireDate = $_POST['HireDate'];
-
-    $sql_update = "UPDATE employee SET EmpName = '$name', Age = '$age', Salary = '$salary', HireDate = '$hireDate' WHERE EmpID = '$EmpID'";
-    
-    if ($conn->query($sql_update) === TRUE) {
-        header("Location: index.php");
-        exit();
-    } else {
-        echo "Error updating employee: " . $conn->error;
-    }
 
 // load employee attributes by id
-} elseif (isset($_POST['EmpID'])) {
+if (isset($_POST['EmpID'])) {
     $EmpID = $_POST["EmpID"];
     $sql_display = "SELECT * FROM employee WHERE EmpID = $EmpID";
     $result = $conn->query($sql_display);
 
     if ($result->num_rows > 0) {
         $emp_row = $result->fetch_assoc();
+
+        // update submission
+        if (isset($_POST['EmpName'], $_POST['Age'], $_POST['Salary'], $_POST['HireDate'])) {
+            $name = $_POST['EmpName'];
+            $age = $_POST['Age'];
+            $salary = $_POST['Salary'];
+            $hireDate = $_POST['HireDate'];
+
+            $sql_update = "UPDATE employee SET EmpName = '$name', Age = '$age', Salary = '$salary', HireDate = '$hireDate' WHERE EmpID = '$EmpID'";
+            
+            if ($conn->query($sql_update) === TRUE) {
+                header("Location: index.php");
+                exit();
+            } else {
+                echo "Error updating employee: " . $conn->error;
+            }
 
         echo "<form action='editEmployee.php' method='post'>".
             "<input type='hidden' name='EmpID' value='".$emp_row['EmpID']."'><br>".
@@ -35,9 +34,11 @@ if (isset($_POST['EmpID'])) {
             "Hire Date: <input type='date' name='HireDate' value='".$emp_row['HireDate']."'><br>".
             "<button type='submit'>Update Employee</button>".
         "</form>";
-    } else {
+        } else {
         echo "Employee not found.";
+        }
     }
 }
+
 $conn->close();
 ?>
