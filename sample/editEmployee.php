b31cb7455d818308bc2e776f70520c2cd4d42679
Andrea Laserna <aalaserna@up.edu.ph>
2025-05-04 00:26:51 +0800

    editEmployee php update final

--- sample/editEmployee.php
+++ sample/editEmployee.php
@@ -1,44 +1,40 @@
 <?php
 include 'DBConnector.php';
 
-// load employee attributes by id
-if (isset($_POST['EmpID'])) {
-    $empID = $_POST["EmpID"];
-    $sql_display = "SELECT * FROM employee WHERE EmpID = $empID";
-    $result = $conn->query($sql_display);
+$emp_id = $_POST['EmpID'];
+$sql_emp_data = "SELECT * FROM employee WHERE EmpID = $emp_id";
+$result = $conn->query($sql_emp_data);
 
-    if ($result->num_rows > 0) {
-        $emp_row = $result->fetch_assoc();
+if ($result->num_rows == 1) {
+    $emp_result = $result->fetch_assoc();
 
-        if (isset($_POST['EmpName'], $_POST['Age'], $_POST['Salary'], $_POST['HireDate'])) {
-            $empName = $_POST['EmpName'];
-            $age = $_POST['Age'];
-            $salary = $_POST['Salary'];
-            $hireDate = $_POST['HireDate'];
+    if (isset($_POST['EmpName'], $_POST['Age'], $_POST['Salary'], $_POST['HireDate'])) {
+        $emp_name = $_POST['EmpName'];
+        $age = $_POST['Age'];
+        $salary = $_POST['Salary'];
+        $hire_date = $_POST['HireDate'];
 
-            $sql_update = "UPDATE employee SET EmpName = '$empName', Age = '$age', Salary = '$salary', HireDate = '$hireDate' WHERE EmpID = '$empID'";
-            
-            if ($conn->query($sql_update) === TRUE) {
-                header("Location: index.php");
-                exit();
-            } else {
-                echo "Error updating employee: " . $conn->error;
-            }
+        $sql_edit_emp = "UPDATE employee SET EmpName = '$emp_name', Age = '$age', Salary = '$salary', HireDate = '$hire_date' WHERE EmpID = '$emp_id'";
+        
+        if ($conn->query($sql_edit_emp)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Error editing employee: " . $conn->error;
         }
-        echo "<form action='editEmployee.php' method='post'>".
-            "<input type='hidden' name='EmpID' value='".$emp_row['EmpID']."'><br>".
-            "Name: <input type='text' name='EmpName' value='".$emp_row['EmpName']."'><br>".
-            "Age: <input type='number' name='Age' value='".$emp_row['Age']."'><br>".
-            "Salary: <input type='number' name='Salary' value='".$emp_row['Salary']."'><br>".
-            "Hire Date: <input type='date' name='HireDate' value='".$emp_row['HireDate']."'><br>".
+    }
+
+    echo "<form action='editEmployee.php' method='post'>".
+            "<input type='hidden' name='EmpID' value='".$emp_result['EmpID']."'><br>".
+            "Name: <input type='text' name='EmpName' value='".$emp_result['EmpName']."'><br>".
+            "Age: <input type='number' name='Age' value='".$emp_result['Age']."'><br>".
+            "Salary: <input type='number' step='.01' name='Salary' value='".$emp_result['Salary']."'><br>".
+            "Hire Date: <input type='date' name='HireDate' value='".$emp_result['HireDate']."'><br>".
             "<button type='submit'>Update Employee</button>".
         "</form>";
-    } else {
-        echo "Employee not found.";
-    }
-    
-} 
-
+} else {
+    echo "Employee not found.";
+}
 
 $conn->close();
 ?>
