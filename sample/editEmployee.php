0d7e5c9bef200453a139b151aebf9c975190ab6c
Andrea Laserna <aalaserna@up.edu.ph>
2025-05-03 20:41:27 +0800

    editEmployee update 3

--- sample/editEmployee.php
+++ sample/editEmployee.php
@@ -3,21 +3,20 @@ include 'DBConnector.php';
 
 // load employee attributes by id
 if (isset($_POST['EmpID'])) {
-    $EmpID = $_POST["EmpID"];
-    $sql_display = "SELECT * FROM employee WHERE EmpID = $EmpID";
+    $empID = $_POST["EmpID"];
+    $sql_display = "SELECT * FROM employee WHERE EmpID = $empID";
     $result = $conn->query($sql_display);
 
     if ($result->num_rows > 0) {
         $emp_row = $result->fetch_assoc();
 
-        // update submission
         if (isset($_POST['EmpName'], $_POST['Age'], $_POST['Salary'], $_POST['HireDate'])) {
-            $name = $_POST['EmpName'];
+            $empName = $_POST['EmpName'];
             $age = $_POST['Age'];
             $salary = $_POST['Salary'];
             $hireDate = $_POST['HireDate'];
 
-            $sql_update = "UPDATE employee SET EmpName = '$name', Age = '$age', Salary = '$salary', HireDate = '$hireDate' WHERE EmpID = '$EmpID'";
+            $sql_update = "UPDATE employee SET EmpName = '$empName', Age = '$age', Salary = '$salary', HireDate = '$hireDate' WHERE EmpID = '$empID'";
             
             if ($conn->query($sql_update) === TRUE) {
                 header("Location: index.php");
@@ -25,7 +24,7 @@ if (isset($_POST['EmpID'])) {
             } else {
                 echo "Error updating employee: " . $conn->error;
             }
-
+        }
         echo "<form action='editEmployee.php' method='post'>".
             "<input type='hidden' name='EmpID' value='".$emp_row['EmpID']."'><br>".
             "Name: <input type='text' name='EmpName' value='".$emp_row['EmpName']."'><br>".
@@ -34,11 +33,12 @@ if (isset($_POST['EmpID'])) {
             "Hire Date: <input type='date' name='HireDate' value='".$emp_row['HireDate']."'><br>".
             "<button type='submit'>Update Employee</button>".
         "</form>";
-        } else {
+    } else {
         echo "Employee not found.";
-        }
     }
-}
+    
+} 
+
 
 $conn->close();
 ?>
